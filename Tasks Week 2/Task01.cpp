27e126a5841363a81e4c2387aa6f87fc946bd311
Malik Bilal <bilalyaseen2k4@gmail.com>
2023-09-19 23:24:31 +0500

    Create Task01.cpp

---
+++ Tasks Week 2/Task01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+main(){
+string name, section;
+float roll_number, aggregate;
+cout<<"Enter your name: ";
+cin>> name;
+cout<<"Enter your roll number: ";
+cin>> roll_number;
+cout<<"Enter your aggregate: ";
+cin>>aggregate;
+cout<<"Enter your section: ";
+cin>> section;
+cout<<"Student Information:"<< endl ;
+cout<<"Name: " <<name<< endl;
+cout<<"Roll Number: " << roll_number<< endl;
+cout<<"Aggregate: " << aggregate<< endl;
+cout<<"Section: " <<section<< endl;
+}
