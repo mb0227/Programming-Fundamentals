79020bb8a363790032d36e807256e9c985cfae10
Malik Bilal <bilalyaseen2k4@gmail.com>
2023-10-04 03:05:02 +0500

    Create task1.cpp

---
+++ Lab Tasks Week 5/task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main(){
+float num1,num2,minimum;
+cout<<"Enter the first number: ";
+cin>>num1;
+cout<<"Enter the second number: ";
+cin>>num2;
+minimum=min(num1,num2);
+cout<<"The minimum of "<<num1<<" and " << num2<<" is: "<<minimum;
+
+} 
